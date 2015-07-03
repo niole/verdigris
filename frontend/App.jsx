3c96094b374f08cea4e3edd355f235b59997208d
Niole Nelson <niolenelson@gmail.com>
2015-07-03 14:21:56 -0400

    Pretty version

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -8,23 +8,30 @@ var sorty    = require('sorty')
 var _ = require('lodash')
 
 var columns = [
-  { name: 'id', title: "#" },
   { name: 'panel' },
   { name: 'circuit'  },
   { name: 'endUse' },
   { name: 'label' },
-  { name: 'load', type: "number" },
-  { name: 'avg15Min', title: "15 Min", type: "number" },
-  { name: 'avg1Hr', title: "1 Hr", type: "number" },
-  { name: 'avg6Hr', title: "6 Hr", type: "number" },
-  { name: 'avg24Hr', title: " 24 Hr", type: "number" }
+  { name: 'load', title: "Load (kW)", type: "number" },
+  { name: 'avg15Min', title: "15 Min (kWh)", type: "number" },
+  { name: 'avg1Hr', title: "1 Hr (kWh)", type: "number" },
+  { name: 'avg6Hr', title: "6 Hr (kWh)", type: "number" },
+  { name: 'avg24Hr', title: " 24 Hr (kWh)", type: "number" }
 ]
 var dataOrig = [
-  { id: 0, panel: "HK", circuit: "7 - Dish", endUse : "cook", label: "..", load: 7.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 96.0 },
-  { id: 1, panel: "AA", circuit: "1 - Dish", endUse : "Acook", label: "A..", load: 2.0, avg15Min: .50, avg1Hr: 1.0, avg6Hr: 2.5, avg24Hr: 6.0 }
+  { id: 0, panel: "HK", circuit: "7 - Dishwsasher", endUse : "Cooking", label: "..", load: 7.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 96.0 },
+  { id: 1, panel: "HK", circuit: "17 - Booster Heater", endUse : "Space Heating", label: "..", load: 5.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 66.7 },
+  { id: 2, panel: "HK", circuit: "9 - Dishwasher", endUse : "Cooking", label: "Dishwasher", load: 4.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 54.5 },
+  { id: 3, panel: "HK", circuit: "11 - Patio Lights", endUse : "Lighting", label: "Exterior", load: 4.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 53.1 },
+  { id: 4, panel: "HK", circuit: "13 - Booster Heater", endUse : "Space Heating", label: "Fan", load: 4.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 52.5 },
+  { id: 5, panel: "HK", circuit: "7 - Dishwasher", endUse : "Cooking", label: "..", load: 7.0, avg15Min: 7.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 96.0 },
+  { id: 6, panel: "HK", circuit: "17 - Booster Heater", endUse : "Space Heating", label: "..", load: 5.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 66.7 },
+  { id: 7, panel: "HK", circuit: "9 - Dishwasher", endUse : "Cooking", label: "..", load: 4.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 54.5 },
+  { id: 9, panel: "HK", circuit: "11 - Dishwasher", endUse : "..", label: "..", load: 4.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 53.1},
+  { id: 10, panel: "HK", circuit: "13 - Booster Heater", endUse : "..", label: "..", load: 4.0, avg15Min: 1.0, avg1Hr: 4.0, avg6Hr: 32.5, avg24Hr: 52.5 },
 ]
 var data = dataOrig
-var SORT_INFO = [ { name: 'id', dir: 'asc'}]
+var SORT_INFO = []
 
 function sort(arr){
   return sorty(SORT_INFO, arr)
@@ -64,11 +71,11 @@ var MySlider = React.createClass({
 var App = React.createClass({
   render: function(){
     return (<div>
+            <h2>Real Time Power (kWh)</h2>
             <div id="slider">
               <MySlider handleSliderChange={this.handleSliderChange} />
             </div>
             <DataGrid
-                idProperty='id'
                 dataSource={data}
                 columns={columns}
                 style={{height: 400}}
